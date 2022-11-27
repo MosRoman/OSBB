ea9744844560044daa9340b6330b25dee5a1646b
MosRoman <morovo1988@gmail.com>
2022-11-27 13:49:39 +0200

    Initial commit

---
+++ src/test/java/com/norovo1988/OSBB/OsbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norovo1988.OSBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
